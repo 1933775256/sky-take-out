76ebda76baf65c7ce7829a65c2273d1407f364a0
zaijian2024 <1933775256@qq.com>
2025-02-26 10:45:18 +0800

    新增员工业务代码

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,5 +1,6 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -7,16 +8,22 @@ import java.io.Serializable;
 @Data
 public class EmployeeDTO implements Serializable {
 
+    @ApiModelProperty(value = "id")
     private Long id;
 
+    @ApiModelProperty(value = "用户名",example = "zhangsan")
     private String username;
 
+    @ApiModelProperty(value = "名字",example = "张三")
     private String name;
 
+    @ApiModelProperty(value = "手机号",example = "11111111111")
     private String phone;
 
+    @ApiModelProperty(value = "性别",example = "男")
     private String sex;
 
+    @ApiModelProperty(value = "身份证号",example = "420117200408060873")
     private String idNumber;
 
 }
